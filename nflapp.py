c88112f329670fe14d31e8d1f6c42005675c9084
demarcomf <85248189+demarcomf@users.noreply.github.com>
2021-11-28 12:29:38 -0500

    Update nflapp.py

--- nflapp.py
+++ nflapp.py
@@ -218,7 +218,7 @@ st.markdown("""---""")
 
 hide_st_style = """
             <style>
-            #MainMenu {visibility: visible;}
+            #MainMenu {visibility: hidden;}
             footer {visibility: hidden;}
             header {visibility: hidden;}
             </style>
